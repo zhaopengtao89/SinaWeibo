058168666fadf847e5b577bee57da17f31780d9c
qingyun <qingyun@qingyun.local>
2014-06-06 14:57:58 +0800

    Initial Commit

---
+++ SinaWeiBo/QYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  QYAppDelegate.h
+//  SinaWeiBo
+//
+//  Created by qingyun on 14-6-6.
+//  Copyright (c) 2014年 HNqingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SinaWeiBo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SinaWeiBo
+//
+//  Created by qingyun on 14-6-6.
+//  Copyright (c) 2014年 HNqingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "QYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([QYAppDelegate class]));
+    }
+}
